50fceef866781501d4502c66cdb75360eeb9164e
phphacker <anatoly.rostovtsev@gmail.com>
2015-10-06 19:25:01 +0300

    test
    test

---
+++ views/header.php
@@ -0,0 +1,15 @@
+<header>
+	<div class="container clearfix">
+		<!-- логотип -->
+		<a href="#" class="logo">Yellow Duck</a>
+		<!-- меню -->
+		<nav>
+			<ul>
+				<li><a href="">О Компании</a></li>
+				<li><a href="#">Каталог</a></li>
+				<li><a href="">Доставка и оплата</a></li>
+				<li><a href="">Контакты</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
